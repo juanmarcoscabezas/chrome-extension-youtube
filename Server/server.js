752e07b39a875f7e5225ebc951b624e740f4c7b0
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-06-21 22:18:35 +0000

    ssl certificate

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,25 @@
-const server = require('http').createServer();
+const fs = require('fs');
+
+const options = {
+	key: fs.readFileSync('key.pem', 'utf8'),
+	cert: fs.readFileSync('server.crt', 'utf8')
+};
+
+//const server = require('http').createServer();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	console.log('Entra');
+	return res.send('Holaa');
+})
+
+app.listen(4000, () => {
+	console.log('Server 2');
+})
+
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 io.on('connection', client => {
@@ -12,6 +33,11 @@ io.on('connection', client => {
 
   client.on('disconnect', () => {console.log('Disconected')});
 });
+
+app.get('/', (req, res) => {
+	console.log('Entra');
+	return res.send('Holaa');
+})
 
 server.listen(3000, () => {
   console.log('Running on port 3000');
